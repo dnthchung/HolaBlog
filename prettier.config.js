9d1abdfe891cade1d47e42513c1548a39cb4c1fa
hblab-account <chungdthe176077@fpt.edu.vn>
2025-10-30 16:36:11 +0700

    mdf

---
+++ prettier.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+// {
+//   "arrowParens": "always",
+//   "bracketSameLine": false,
+//   "bracketSpacing": true,
+//   "embeddedLanguageFormatting": "auto",
+//   "htmlWhitespaceSensitivity": "css",
+//   "insertPragma": false,
+//   "jsxSingleQuote": false,
+//   "printWidth": 200,
+//   "proseWrap": "preserve",
+//   "quoteProps": "as-needed",
+//   "requirePragma": false,
+//   "semi": true,
+//   "singleQuote": false,
+//   "tabWidth": 2,
+//   "trailingComma": "all",
+//   "useTabs": false,
+//   "vueIndentScriptAndStyle": false
+// }
