76dae547caefa8702e470118a473544e814822ee
ChungDT-Personal <chungdthe176077@fpt.edu.vn>
2025-12-07 03:17:26 +0700

    style: format with prettier

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,21 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'HolaBlog',
-  author: 'Doan Thanh Chung',
-  headerTitle: 'HolaBlog',
-  description: 'My life with coding, language and photography',
-  language: ['en-us', 'vi-vn'],
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://hola-blog.vercel.app',
-  siteRepo: 'https://github.com/dnthchung/hola-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  email: 'chungdt03.work@gmail.com',
-  github: 'https://github.com/dnthchung',
-  x: 'https://twitter.com/x',
-  youtube: 'https://www.youtube.com/@dnthchung',
-  linkedin: 'https://www.linkedin.com/in/chungdt03/',
-  locale: ['en-US', 'vi-VN'],
+  title: "HolaBlog",
+  author: "Doan Thanh Chung",
+  headerTitle: "HolaBlog",
+  description: "My life with coding, language and photography",
+  language: ["en-us", "vi-vn"],
+  theme: "system", // system, dark or light
+  siteUrl: "https://hola-blog.vercel.app",
+  siteRepo: "https://github.com/dnthchung/hola-blog",
+  siteLogo: `${process.env.BASE_PATH || ""}/static/images/logo.png`,
+  socialBanner: `${process.env.BASE_PATH || ""}/static/images/twitter-card.png`,
+  email: "chungdt03.work@gmail.com",
+  github: "https://github.com/dnthchung",
+  x: "https://twitter.com/x",
+  youtube: "https://www.youtube.com/@dnthchung",
+  linkedin: "https://www.linkedin.com/in/chungdt03/",
+  locale: ["en-US", "vi-VN"],
   // set to true if you want a navbar fixed to the top
   stickyNav: true,
   analytics: {
@@ -45,14 +45,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: "buttondown",
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: "giscus", // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -60,27 +60,27 @@ const siteMetadata = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      mapping: "pathname", // supported options: pathname, url, title
+      reactions: "1", // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: "0",
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: "light",
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: "transparent_dark",
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
+      themeURL: "",
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: "en",
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
+    provider: "kbar", // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+      searchDocumentsPath: `${process.env.BASE_PATH || ""}/search.json`, // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
@@ -91,6 +91,6 @@ const siteMetadata = {
     //   indexName: 'docsearch',
     // },
   },
-}
+};
 
-module.exports = siteMetadata
+module.exports = siteMetadata;
--- prettier.config.js
+++ prettier.config.js
@@ -4,10 +4,10 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   bracketSpacing: true,
-  plugins: ['prettier-plugin-tailwindcss'],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
 
 // {
 //   "arrowParens": "always",
